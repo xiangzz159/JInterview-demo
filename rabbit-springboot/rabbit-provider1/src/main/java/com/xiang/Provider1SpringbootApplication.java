beca86f0a1d99c9eba79c428433cdb5e82329690
xiangguangye <xiangguangye@sunline.com>
2021-05-27 20:43:05 +0800

    commit

---
+++ rabbit-springboot/rabbit-provider1/src/main/java/com/xiang/Provider1SpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.xiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class Provider1SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Provider1SpringbootApplication.class, args);
+	}
+
+}

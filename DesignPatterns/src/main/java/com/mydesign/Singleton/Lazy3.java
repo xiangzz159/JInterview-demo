53f99803557565bb9d2b96d62622ad2824411f8e
xiangguangye <xiangguangye@sunline.com>
2021-06-16 22:51:17 +0800

    commit

--- DesignPatterns/src/main/java/com/mydesign/Singleton/Lazy3.java
+++ DesignPatterns/src/main/java/com/mydesign/Singleton/Lazy3.java
@@ -5,9 +5,10 @@ package com.mydesign.Singleton;
  * @Date：2020/11/8 13:52
  * @Desc：双重检查锁(DCL)
  * 综合了懒汉式和饿汉式两者的优缺点整合而成。看上面代码实现中，特点是在synchronized关键字内外都加了一层 if 条件判断，这样既保证了线程安全，又比直接上锁提高了执行效率，还节省了内存空间。
+ * 使用volatile修饰变量能防止系统指令重排。
  */
 public class Lazy3 {
-    private static Lazy3 instance;
+    private static volatile Lazy3 instance;
 
     private Lazy3(){}
 
